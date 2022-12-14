51165403f543d911e45cdf4c16e88c9a5b74f874
matthieu <campagne.matthieu@gmail.com>
2022-12-14 11:55:31 +0100

    Update TP

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,16 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import Formik from "./components/Formik.vue";
+const submit = (data) => {
+  console.log("debug here", data);
+};
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <Formik @submitFinal="submit" :validate="() => {}" v-slot="{ handleSubmit }">
+      <form @submit.prevent="handleSubmit">
+      <button type="submit">submit</button>
+    </form>
+    </Formik>
   </main>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
---
+++ src/components/Formik.vue
@@ -0,0 +1,16 @@
+<script setup>
+  const $emit = defineEmits(["submitFinal"]);
+  const props = defineProps({
+    validate: {
+      type: Function,
+      required: true,
+    },
+  });
+  const handleSubmit = () => {
+    $emit("submitFinal", "wdrjhkfgqze ");
+  };
+</script>
+
+<template>
+  <slot :handleSubmit="handleSubmit"></slot>
+</template>
